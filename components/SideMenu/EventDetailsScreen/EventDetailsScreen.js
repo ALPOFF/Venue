efa8248b9f5497e89712345d188b042a534b00a8
ALPOFF <leha.mms82@gmail.com>
2020-07-01 03:28:59 +0300

    added carousel

--- components/SideMenu/EventDetailsScreen/EventDetailsScreen.js
+++ components/SideMenu/EventDetailsScreen/EventDetailsScreen.js
@@ -1,5 +1,16 @@
 import React, {useEffect, useState} from "react";
-import {View, Text, Button, TouchableOpacity, ScrollView, Image, AsyncStorage} from "react-native";
+import {
+    View,
+    Text,
+    Button,
+    TouchableOpacity,
+    Dimensions,
+    StyleSheet,
+    ScrollView,
+    Image,
+    AsyncStorage,
+    SafeAreaView
+} from "react-native";
 import {Icon} from "react-native-elements";
 import userPic from '../../../assets/Screenshot_6.png'
 import ImageBackground from "react-native-web/dist/exports/ImageBackground";
@@ -8,74 +19,140 @@ import EventVisitors from "./EventVisitors/EventVisitors";
 import EventVisitorsTwo from "./EventVisitors/EventVisitorsTwo";
 import EventVisitorsOne from "./EventVisitors/EventVisitorsOne";
 import {connect} from "react-redux";
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import AnimatedWithChildren from "react-native-web/dist/vendor/react-native/Animated/nodes/AnimatedWithChildren";
+const { height, width } = Dimensions.get('window');
 
-const EventDetailsScreen = (props) => {
-    const postId = props.navigation.state.params.postId;
-    const userIdOrg = props.navigation.state.params.userId;
-    const postText = props.navigation.state.params.postText;
-    const pic = props.navigation.state.params.pic;
-    const visitors = props.navigation.state.params.visitors;
+class EventDetailsScreen extends React.Component<{}> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                'http://www.bluecode.jp/images/A.png',
+                'http://www.bluecode.jp/images/B.png',
+                 'http://www.bluecode.jp/images/C.png'
 
-    const [currentUserId, setCurrentUserId] = useState(false);
 
-    useEffect(() => {
+            ],
+            currentUserId: false,
+            postId: this.props.navigation.state.params.postId,
+            userIdOrg: this.props.navigation.state.params.userId,
+            postText: this.props.navigation.state.params.postText,
+            pic: this.props.navigation.state.params.pic,
+            visitors: this.props.navigation.state.params.visitors,
+            activeSlide: 0
+        };
+    }
+
+    componentDidMount() {
+        console.log('honepics:', this.state.pic)
         AsyncStorage.getItem('userToken', (err, item) => {
-            setCurrentUserId(item)
-        })
-    }, []);
-
-    const iGo = () => {
-        console.log("NICE")
-        AsyncStorage.getItem('userToken', (err, item) => {
-            axios.post(`https://warm-ravine-29007.herokuapp.com/igo`,
-                {user_id: item, postId: postId});
+            this.setState({'currentUserId': item})
         })
     }
 
-    return (
-        <View style={{display: 'flex', flexDirection: 'column', padding: 10}}>
-            <Image
-                style={{width: '100%', height: 200, borderRadius: 8}}
-                source={{uri: pic}}
-            />
-            <Text>Организатор: {userIdOrg}</Text>
-            <View
-                style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between',}}>
-                <TouchableOpacity style={{display: 'flex', flexDirection: 'row'}}
-                                  onPress={() => props.navigation.navigate('EventVisitorsDetailed', {
-                                      visitors: visitors
-                                  })}>
-                    {visitors.length >= 3 && <EventVisitors visitors={visitors}/>}
-                    {visitors.length === 2 && <EventVisitorsTwo visitors={visitors}/>}
-                    {visitors.length === 1 && <EventVisitorsOne visitors={visitors}/>}
-                </TouchableOpacity>
-                {visitors.some(v => currentUserId == v) ?
-                    <View style={{margin: 10, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                        <Text style={{fontWeight: 'bold', fontSize: 15, color: 'grey', margin: 10}}>Вы идете</Text>
+    renderItem = ({item, index}) => {
+        return (
+            <Image style={styles.logoStyle} source={{ uri: item }} />
+        );
+    }
+
+    render() {
+
+        const iGo = () => {
+            console.log("NICE")
+            AsyncStorage.getItem('userToken', (err, item) => {
+                axios.post(`https://warm-ravine-29007.herokuapp.com/igo`,
+                    {user_id: item, postId: this.state.postId});
+            })
+        }
+
+        return (
+            <View style={{display: 'flex', flexDirection: 'column', paddingLeft: 10, paddingRight: 10}}>
+                <View >
+                    <View style={{
+
+
+
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <SafeAreaView style={{ height: 240 }}>
+                        <Carousel
+                            inactiveSlideOpacity={0.6}
+                            inactiveSlideScale={0.95}
+                            layoutCardOffset={`18`}
+                            firstItem={0}
+                            itemWidth={width}
+                            sliderWidth={width}
+                            onSnapToItem={index => this.setState({ activeSlide: index })} //for pagination
+                            layout={'stack'}
+                            data={this.state.pic}
+                            renderItem={this.renderItem}
+                            //contentContainerCustomStyle={{ alignItems: 'center' }}
+                        />
+                        <Pagination
+                            dotsLength={this.state.pic.length} //dotの数
+                            activeDotIndex={this.state.activeSlide} //どのdotをactiveにするか
+                            containerStyle={{paddingVertical:15}} //デフォルトではちと広い
+                        />
+                        </SafeAreaView>
                     </View>
-                    :
+                </View>
+
+                <Text>Организатор: {this.state.userIdOrg}</Text>
+                <View
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}>
                     <TouchableOpacity style={{display: 'flex', flexDirection: 'row'}}
-                                      onPress={() => iGo()}>
+                                      onPress={() => this.props.navigation.navigate('EventVisitorsDetailed', {
+                                          visitors: this.state.visitors
+                                      })}>
+                        {this.state.visitors.length >= 3 && <EventVisitors visitors={this.state.visitors}/>}
+                        {this.state.visitors.length === 2 && <EventVisitorsTwo visitors={this.state.visitors}/>}
+                        {this.state.visitors.length === 1 && <EventVisitorsOne visitors={this.state.visitors}/>}
+                    </TouchableOpacity>
+                    {this.state.visitors.some(v => this.state.currentUserId == v) ?
                         <View style={{margin: 10, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                            <Text style={{fontWeight: 'bold', fontSize: 15, color: 'black', margin: 10}}>Я пойду!</Text>
+                            <Text style={{fontWeight: 'bold', fontSize: 15, color: 'grey', margin: 10}}>Вы идете</Text>
+                        </View>
+                        :
+                        <TouchableOpacity style={{display: 'flex', flexDirection: 'row'}}
+                                          onPress={() => iGo()}>
+                            <View style={{margin: 10, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                <Text style={{fontWeight: 'bold', fontSize: 15, color: 'black', margin: 10}}>Я
+                                    пойду!</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+
+                    <TouchableOpacity style={{display: 'flex', flexDirection: 'row'}}
+                                      onPress={() => this.props.navigation.navigate('Dialog', {
+                                          msg: 'Some Msg'
+                                      })}>
+                        <View style={{margin: 10, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                            <Text style={{fontWeight: 'bold', fontSize: 15, color: 'black', margin: 10}}>Перейти в
+                                беседу</Text>
                         </View>
                     </TouchableOpacity>
-                }
-
-                <TouchableOpacity style={{display: 'flex', flexDirection: 'row'}}
-                                  onPress={() => props.navigation.navigate('Dialog', {
-                                      msg: 'Some Msg'
-                                  })}>
-                    <View style={{margin: 10, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                        <Text style={{fontWeight: 'bold', fontSize: 15, color: 'black', margin: 10}}>Перейти в
-                            беседу</Text>
-                    </View>
-                </TouchableOpacity>
+                </View>
+                <Text>{this.state.postText}</Text>
+                <Text>{this.state.postId}</Text>
             </View>
-            <Text>{postText}</Text>
-            <Text>{postId}</Text>
-        </View>
-    )
+        )
+    }
+}
+const styles = {
+    logoStyle: {
+        width: width,
+        height: width/2
+    }
 };
+
+
 
 export default connect(null, {})(EventDetailsScreen);
