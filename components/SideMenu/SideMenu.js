6e9b5ee41e1a887a450e651ee132f6bb25e09200
ALPOFF <leha.mms82@gmail.com>
2021-04-13 15:45:55 +0300

    fixed backgr pic and prof pic in sidemenu component

--- components/SideMenu/SideMenu.js
+++ components/SideMenu/SideMenu.js
@@ -7,6 +7,7 @@ import { Icon } from "react-native-elements";
 import { connect } from "react-redux";
 import { setUserProfileBarThunk } from "../../state/appReducer";
 import { localizeSideMenuScreen } from "../../localization/localize";
+import { ImageBackground } from 'react-native';
 
 class SideMenu extends Component {
     constructor(props) {
@@ -43,47 +44,86 @@ class SideMenu extends Component {
             <View style={styles.container}>
                 <ScrollView showsVerticalScrollIndicator={true} decelerationRate={"normal"}>
                     {this.props.userProfileBar.length != 0 && <View style={{
-                        margin: 10,
-                        paddingBottom: 15,
+                        // margin: 10,
+                        // paddingBottom: 15,
                         borderBottomWidth: 1,
                         borderBottomColor: '#009788'
                     }}>
-                        {/*{this.props.userProfileBar[0] != undefined && <ImageBackground source={{uri: this.props.userProfileBar[0].background_pic}} style={{height: 40}}>*/}
-                        <View style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignItems: 'center'
-                        }}>
-                            <Image source={{ uri: this.props.userProfileBar[0].photo }}
-                                style={{ height: 60, width: 60, borderRadius: 40 }} />
-                            <View style={{ display: "flex", flexDirection: "column", paddingLeft: 10 }}>
-                                <Text style={{
-                                    fontWeight: 'bold',
-                                    fontSize: 20,
-                                    color: 'black'
-                                }}>{this.props.userProfileBar[0].name}</Text>
-                                <Text style={{
-                                    fontSize: 18,
-                                    color: 'lightgrey'
-                                }}>@{this.props.userProfileBar[0].Username}</Text>
+                        {this.props.userProfileBar[0].background_pic != null ? <ImageBackground source={{ uri: this.props.userProfileBar[0].background_pic }} style={{ height: 90 }}>
+                            <View style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                height: 120
+                            }}>
+                                {this.props.userProfileBar[0].photo !== null ?
+                                    <Image source={{ uri: this.props.userProfileBar[0].photo }}
+                                        style={{ height: 60, width: 60, borderRadius: 40 }} />
+                                    : <Image source={{ uri: 'https://eshendetesia.com/images/user-profile.png' }}
+                                        style={{ height: 60, width: 60, borderRadius: 40 }} />
+                                }
                             </View>
-                        </View>
-                        {/*<View style={{paddingTop: 10}}>*/}
-                        {/*    <Text style={{*/}
-                        {/*        fontWeight: 'bold',*/}
-                        {/*        fontFamily: 'Oxygen-Bold',*/}
-                        {/*        fontSize: 16,*/}
-                        {/*        color: '#14171A',*/}
-                        {/*    }}>{localizeSideMenuScreen.SubscribersText}: {this.props.userProfileBar[0].subscribers.length}</Text>*/}
-                        {/*    <Text style={{*/}
-                        {/*        fontWeight: 'bold',*/}
-                        {/*        fontFamily: 'Oxygen-Bold',*/}
-                        {/*        fontSize: 16,*/}
-                        {/*        color: '#14171A',*/}
-                        {/*    }}>{localizeSideMenuScreen.SubscriptionsText}: {this.props.userProfileBar[0].subscribes.length}</Text>*/}
-                        {/*</View>*/}
-                        {/*</ImageBackground>}*/}
+                            {/*<View style={{paddingTop: 10}}>*/}
+                            {/*    <Text style={{*/}
+                            {/*        fontWeight: 'bold',*/}
+                            {/*        fontFamily: 'Oxygen-Bold',*/}
+                            {/*        fontSize: 16,*/}
+                            {/*        color: '#14171A',*/}
+                            {/*    }}>{localizeSideMenuScreen.SubscribersText}: {this.props.userProfileBar[0].subscribers.length}</Text>*/}
+                            {/*    <Text style={{*/}
+                            {/*        fontWeight: 'bold',*/}
+                            {/*        fontFamily: 'Oxygen-Bold',*/}
+                            {/*        fontSize: 16,*/}
+                            {/*        color: '#14171A',*/}
+                            {/*    }}>{localizeSideMenuScreen.SubscriptionsText}: {this.props.userProfileBar[0].subscribes.length}</Text>*/}
+                            {/*</View>*/}
+                        </ImageBackground>
+                            : <ImageBackground source={{ uri: 'https://wallpapercave.com/wp/wp2445766.jpg' }} style={{ height: 120 }}>
+                                <View style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    height: 120
+                                }}>
+                                    {this.props.userProfileBar[0].photo !== null ?
+                                        <Image source={{ uri: this.props.userProfileBar[0].photo }}
+                                            style={{ height: 60, width: 60, borderRadius: 40 }} />
+                                        : <Image source={{ uri: 'https://eshendetesia.com/images/user-profile.png' }}
+                                            style={{ height: 60, width: 60, borderRadius: 40 }} />
+                                    }
+                                </View>
+                                {/*<View style={{paddingTop: 10}}>*/}
+                                {/*    <Text style={{*/}
+                                {/*        fontWeight: 'bold',*/}
+                                {/*        fontFamily: 'Oxygen-Bold',*/}
+                                {/*        fontSize: 16,*/}
+                                {/*        color: '#14171A',*/}
+                                {/*    }}>{localizeSideMenuScreen.SubscribersText}: {this.props.userProfileBar[0].subscribers.length}</Text>*/}
+                                {/*    <Text style={{*/}
+                                {/*        fontWeight: 'bold',*/}
+                                {/*        fontFamily: 'Oxygen-Bold',*/}
+                                {/*        fontSize: 16,*/}
+                                {/*        color: '#14171A',*/}
+                                {/*    }}>{localizeSideMenuScreen.SubscriptionsText}: {this.props.userProfileBar[0].subscribes.length}</Text>*/}
+                                {/*</View>*/}
+                            </ImageBackground>
+                        }
                     </View>}
+                    <View style={{ display: "flex", paddingLeft: 40, flexDirection: "column", marginLeft: 10, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        {/* <View style={{ width: 30 }}><Icon name="ios-settings" type='ionicon' size={25}
+                            color='#263238' /></View> */}
+                        {this.props.userProfileBar[0].name != null && <Text style={{
+                            fontWeight: 'bold',
+                            fontSize: 20,
+                            color: 'black'
+                        }}>{this.props.userProfileBar[0].name}</Text>}
+                        <Text style={{
+                            fontSize: 18,
+                            color: 'black'
+                        }}>@{this.props.userProfileBar[0].Username}</Text>
+                    </View>
                     <TouchableOpacity style={{ display: 'flex', flexDirection: 'row' }}
                         onPress={() => this.props.navigation.navigate('switchNavEditProfile', { user_id: this.state.user_id })}>
                         <View style={{ marginLeft: 10, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
--- components/SideMenu/SideMenu.style.js
+++ components/SideMenu/SideMenu.style.js
@@ -1,6 +1,6 @@
 export default {
   container: {
-    paddingTop: 20,
+    // paddingTop: 20,
     flex: 1
   },
   navItemStyle: {
